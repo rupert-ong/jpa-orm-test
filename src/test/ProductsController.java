bcee71ca15755377c46bab03a034fd4fc6f6f4e4
Rupert Ong <rupert.ong@gmail.com>
2016-07-05 11:28:06 -0400

    feat: Add read entry by id functionality

--- src/test/ProductsController.java
+++ src/test/ProductsController.java
@@ -19,7 +19,7 @@ public class ProductsController extends HttpServlet{
     System.err.println(qString);
 
     if(req.getParameter("id") != null) {
-      sendResponse(req, res, qString);
+      findProduct(req, res);
     } else if(req.getParameter("all") != null) {
       sendResponse(req, res, qString);
     } else {
@@ -27,6 +27,53 @@ public class ProductsController extends HttpServlet{
     }
   }
 
+  /**
+   * Search product table (via Product Entity class) by id and send page response
+   * @param req HttpServletRequest
+   * @param res HttpServletResponse
+   */
+  private void findProduct(HttpServletRequest req, HttpServletResponse res) {
+    EntityManager em = getEM(req);
+
+    Product prod = null;
+    String msg = "No product found";
+
+    try{
+      long id = Integer.parseInt(req.getParameter("id").trim());
+      prod = em.find(Product.class, id);
+      if(prod != null) {
+        msg = "Product " + id + " is named " + prod.getProductName() + ".";
+      }
+    } 
+    catch(NumberFormatException e){}
+    catch(IllegalArgumentException e){}
+
+    sendResponse(req, res, msg);
+  }
+
+  /**
+   * Get session stored Entity Manager (or create if null)
+   * @param  req HttpServlet
+   * @return     EntityManager instance (stored in a session)
+   */
+  private EntityManager getEM(HttpServletRequest req) {
+    HttpSession session = req.getSession();
+    EntityManager em = (EntityManager) session.getAttribute(emKey);
+
+    if(em == null) {
+      em = new PersistenceManager().getEntityManager();
+      session.setAttribute(emKey, em);
+    }
+
+    return em;
+  }
+
+  /**
+   * Redirect to a confirmation page with a string to output
+   * @param req HttpServletRequest
+   * @param res HttpServletResponse
+   * @param msg String to output
+   */
   private void sendResponse(HttpServletRequest req, HttpServletResponse res, String msg) {
     HttpSession session = req.getSession();
     session.setAttribute("msg", msg);
